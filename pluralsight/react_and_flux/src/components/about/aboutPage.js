492e93c7eda76a818489579f364496b67c3f1b12
rcornacchia <rlc2160@columbia.edu>
2016-05-24 21:55:17 -0400

    added about page

--- pluralsight/react_and_flux/src/components/about/aboutPage.js
+++ pluralsight/react_and_flux/src/components/about/aboutPage.js
@@ -1,3 +1,25 @@
 "use strict";
 
-var About
+var React = require('react');
+
+var About = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This application uses the following technologies:
+                    <ul>
+                        <li>React</li>
+                        <li>React Router</li>
+                        <li>Flux</li>
+                        <li>Node</li>
+                        <li>Gulp</li>
+                        <li>Browserify</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </p>
+            </div>
+        );
+    }
+});
