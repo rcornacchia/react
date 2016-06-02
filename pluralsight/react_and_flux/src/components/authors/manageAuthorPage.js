105e20590e4d69216e94620fd5ea38b5c3492fbe
rcornacchia <rlc2160@columbia.edu>
2016-06-01 21:26:42 -0400

    added manageAuthorPage

---
+++ pluralsight/react_and_flux/src/components/authors/manageAuthorPage.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var React = require('react');
+
+var ManageAuthorPage = React.createClass({
+    render: function() {
+        return (
+            <h1>ManageAuthorPage</h1>
+        );
+    }
+});
+
+module.exports = ManageAuthorPage;
--- pluralsight/react_and_flux/src/routes.js
+++ pluralsight/react_and_flux/src/routes.js
@@ -4,12 +4,19 @@ var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
       <DefaultRoute handler={require('./components/homePage')} />
       <Route name="authors" handler={require('./components/authors/authorPage')} />
+      <route name="addAuthor" path="auhtor" handler={require('./components/authors/manageAuthorPage')};
       <Route name="about" handler={require('./components/about/aboutPage')} />
+      <NotFoundRoute handler={require('./components/notFoundPage')} />
+      <Redirect from="about-us" to="about" />
+      <Redirect from="awthurs" to="authors" />
+      <Redirect from="about/*" to="about" />
     </Route>
 );
 
